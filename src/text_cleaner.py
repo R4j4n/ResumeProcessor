f20d1fad5f25ed0340e6b374da0f7a8ba2563beb
PrajwalLuitel <prajwalluitel@gmail.com>
2024-04-17 19:57:43 -0400

    text cleaning issues fixed

--- src/text_cleaner.py
+++ src/text_cleaner.py
@@ -2,7 +2,6 @@ from typing import Any
 import re
 import string
 import nltk
-from nltk.stem import PorterStemmer
 from nltk.corpus import stopwords
 
 
@@ -17,9 +16,8 @@ class TextCleaner:
 
         punctuations_removed = self.remove_punctuations(emojis_cleared)
 
-        stemmed_text = self.stemming(punctuations_removed)
-
-        return stemmed_text
+        cleaned_text = self.remove_stopwords(punctuations_removed)
+        return cleaned_text
     
     def remove_emojis_and_unrecognizable_characters(self,text):
         article = re.sub(r'\([^)]*\)', r'', text)
@@ -27,7 +25,6 @@ class TextCleaner:
         article = re.sub(r'<[^>]*>', r'', article)
         article = re.sub(r'^https?:\/\/.[\r\n]', '', article)
         article = re.sub('http[s]?://\S+', '', article)
-        article = re.sub(r'[a-zA-Z0-9]+', '', article)
         article = re.sub('[!+*-@,#%(&$_?.^]', '', article)
         article = re.sub(' +', ' ',article)
         article = article.replace(u'\ufeff','')
@@ -43,15 +40,17 @@ class TextCleaner:
             result += "" if character in string.punctuation else character
         return result
 
-    def stemming(self,text):
+    def remove_stopwords(self,text):
         text = text.lower()
         words = text.split()
         stop_words = set(stopwords.words('english'))
         words = [word for word in words if word not in stop_words]
 
-        stemmer = PorterStemmer()
-        words = [stemmer.stem(word) for word in words]
-
         text = ' '.join(words)
-
         return text
+
+
+words = "John Doe is an experienced software engineer with a strong background in developing scalable web applications. His technical skills include proficiency in JavaScript, Python, and Java, as well as a deep understanding of React and Node.js frameworks. John has worked on various projects that required collaborative efforts and has consistently delivered high-quality code. He is also familiar with agile methodologies and has participated in numerous sprint planning sessions and daily stand-ups. In addition to his technical abilities, John has demonstrated strong problem-solving skills and the ability to adapt to new technologies quickly. He is a team player who is always eager to learn and contribute to project success."
+cleaner = TextCleaner()
+
+print(f"The cleaned text is as follows:\n\n\t{cleaner(words)}")
